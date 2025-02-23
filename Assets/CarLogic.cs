7d96be8d57c721641f1183325ad9bd0d48490041
LeolucaToppazzini <114596549+LeolucaToppazzini@users.noreply.github.com>
2025-02-23 22:30:55 +0100

    CarLogic - car movements

--- Assets/CarLogic.cs
+++ Assets/CarLogic.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CarLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float moveAmount = Input.GetAxis("Vertical")* moveSpeed;
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        transform.Translate(0, moveAmount, 0);
+        transform.Rotate(0, 0, -steerAmount);
     }
 }
