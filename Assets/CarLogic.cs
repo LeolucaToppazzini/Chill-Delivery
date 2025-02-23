7d6f87cb531745c42d73d348bdf69e86a380014e
LeolucaToppazzini <114596549+LeolucaToppazzini@users.noreply.github.com>
2025-02-23 23:20:47 +0100

    CarLogic - testing collisions

--- Assets/CarLogic.cs
+++ Assets/CarLogic.cs
@@ -13,8 +13,8 @@ public class CarLogic : MonoBehaviour
 
     void Update()
     {
-        float moveAmount = Input.GetAxis("Vertical")* moveSpeed;
-        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical")* moveSpeed * Time.deltaTime;
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         transform.Translate(0, moveAmount, 0);
         transform.Rotate(0, 0, -steerAmount);
     }
