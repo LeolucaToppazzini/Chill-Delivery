a5fba793c81885220ee2680d474e34ed9221d70f
LeolucaToppazzini <114596549+LeolucaToppazzini@users.noreply.github.com>
2025-06-25 18:41:20 +0200

    Collision - triggererEnter

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-  void OnCollisionEnter2D(Collision2D other) {
-    Debug.Log("ship ship");
+  // This function is called when the object collides with another object
+  void OnCollisionEnter2D(Collision2D other)
+  {
+    Debug.Log("collision");
+  }
+
+  // This function is called when the object enters a trigger collider
+  void OnTriggerEnter2D(Collider2D other) {
+    Debug.Log("triggered");
   }
 }
